b9b336d8da7a9ec196760c99461f912196495df1
Yuvrajsinh Vala <yuvrajsinh.vala@letsnurture.com>
2017-02-16 15:32:07 +0530

    Second Commit

---
+++ TestBuddyBuild/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  TestBuddyBuild
+//
+//  Created by ln-mac-01 on 16/02/17.
+//  Copyright © 2017 ln-mac-01. All rights reserved.
+//
+//
+//  ViewController.swift
+//  TestBuddyBuild
+//
+//  Created by ln-mac-01 on 16/02/17.
+//  Copyright © 2017 ln-mac-01. All rights reserved.
+////
+//  ViewController.swift
+//  TestBuddyBuild
+//
+//  Created by ln-mac-01 on 16/02/17.
+//  Copyright © 2017 ln-mac-01. All rights reserved.
+//
+
+
+
+
+
+
+
+
+import UIKit
+
+
+
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view, typically from a nib.// Do any additional setup after loading the view, typically from a nib.// Do any additional setup after loading the view, typically from a nib.// Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
